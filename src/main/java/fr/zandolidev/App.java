03fb016938c170e74fa1bc9470c12529c31cd06a
Didier Erin <didier.erin@erintek.com>
2024-05-23 00:46:42 +0200

    concepts de base : structures de contrôle - ajout du switch/case

--- src/main/java/fr/zandolidev/App.java
+++ src/main/java/fr/zandolidev/App.java
@@ -15,19 +15,9 @@ public class App
 
         // Informations sur le rendez-vous
         String nomMedecin = "Dr. Chen Wong";
-        String specialite = "Cardiologue";
+        String specialite = "Cardiologue"; // Peut être "Cardiologue", "Dermatologue", "Pédiatre"
         int dureeRendezVous = 45; // en minutes
-        double coutRendezVous = 80.0; // en euros
-
-        // Affichage des informations du patient et du rendez-vous
-        System.out.printf("Bienvenue sur DoctoCrest !%n");
-        System.out.printf("Patient : %s", nomPatient);
-        System.out.printf("Âge : %d ans%n", agePatient);
-        System.out.printf("Taille : %.2f mètres%n", taillePatient);
-        System.out.printf("A un rendez-vous : %b%n", aRendezVous);
-        System.out.printf("Médecin : %s (%s)%n", nomMedecin, specialite);
-        System.out.printf("Durée du rendez-vous : %d minutes%n", dureeRendezVous);
-        System.out.printf("Coût du rendez-vous : %.2f euros%n", coutRendezVous);
+        double coutRendezVous;
 
         // Les structures de contrôle
 
@@ -39,6 +29,33 @@ public class App
         } else {
             System.out.println("Le patient est un mineur.");
         }
+
+        // Instruction conditionnelle switch-case
+
+        switch (specialite) {
+            case "Cardiologue":
+                coutRendezVous = 100.0;
+                break;
+            case "Dermatologue":
+                coutRendezVous = 80.0;
+                break;
+            case "Pédiatre":
+                coutRendezVous = 90.0;
+                break;
+            default:
+                coutRendezVous = 70.0;
+                break;
+        }
+
+        // Affichage des informations du patient et du rendez-vous
+        System.out.printf("Bienvenue sur DoctoCrest !%n");
+        System.out.printf("Patient : %s", nomPatient);
+        System.out.printf("Âge : %d ans%n", agePatient);
+        System.out.printf("Taille : %.2f mètres%n", taillePatient);
+        System.out.printf("A un rendez-vous : %b%n", aRendezVous);
+        System.out.printf("Médecin : %s (%s)%n", nomMedecin, specialite);
+        System.out.printf("Durée du rendez-vous : %d minutes%n", dureeRendezVous);
+        System.out.printf("Coût du rendez-vous : %.2f euros%n", coutRendezVous);
 
         // Boucle for - Afficher les 3 prochains rendez-vous
         System.out.println("Les 3 prochains rendez-vous :");
@@ -63,28 +80,12 @@ public class App
         } while (nouveauxRendezVous < 3);
         System.out.printf("%d nouveaux rendez-vous ont été programmés.%n", nouveauxRendezVous);
 
-        // Les opérateurs arithmétiques
-        int nombreDeRendezVous = 3;
-        double coutTotalAnnee = nombreDeRendezVous * coutRendezVous;
-        System.out.println("Le coût total des rendez-vous cette année est de " + coutTotalAnnee + " euros.");
-
-        // Les opérateurs de comparaison
+        // Opérateur ternaire
         int ageMinimumPourTraitement = 18;
         String messagePatientEligible = "Le patient est éligible pour le traitement.";
         String messagePatientNonEligible = "Le patient n'est pas éligible pour le traitement.";
         boolean estEligiblePourTraitement = agePatient >= ageMinimumPourTraitement;
-        String messageEligibilitePatient = estEligiblePourTraitement ? messagePatientEligible : messagePatientNonEligible; // Opérateur ternaire
+        String messageEligibilitePatient = estEligiblePourTraitement ? messagePatientEligible : messagePatientNonEligible;
         System.out.println(messageEligibilitePatient);
-
-        // Les opérateurs logiques
-        boolean aRendezVousAujourdhui = true;
-        boolean aRendezVousEtEstEligible = aRendezVousAujourdhui && estEligiblePourTraitement;
-        System.out.println("Le patient a-t-il un rendez-vous aujourd'hui et est-il éligible pour le traitement ? " + aRendezVousEtEstEligible);
-
-        // Les opérateurs d'incrémentation
-        rendezVousRestants = 5;
-        System.out.println("Nombre de rendez-vous restants aujourd'hui : " + rendezVousRestants);
-        rendezVousRestants--;
-        System.out.println("Un rendez-vous vient de se terminer. Rendez-vous restants : " + rendezVousRestants);
     }
 }
