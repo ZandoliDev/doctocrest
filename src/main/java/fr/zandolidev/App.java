59f8ec751ab9891a57b9eb5a4f005367de58a66c
Didier Erin <didier.erin@erintek.com>
2024-05-29 23:25:55 +0200

    concepts de base : traitement en masse * calcul du temps de traitement

--- src/main/java/fr/zandolidev/App.java
+++ src/main/java/fr/zandolidev/App.java
@@ -1,37 +1,34 @@
 package fr.zandolidev;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
 
         Random random = new Random();
-        for(int i=0; i<10000000; i++) {
-            afficherMessageDeBienvenue();
 
-            String[] nomsPraticiens = {"Dr. Alice Martin", "Dr. Li Wei", "Dr. Maria Gonzalez", "Dr. John Smith", "Dr. Adebayo Adeyemi", "Dr. Hiroshi Yamamoto", "Dr. Elena Ivanova"};
-            String[] specialitesPraticiens = {"Cardiologue", "Dermatologue", "Pédiatre", "Neurologue", "Gynécologue", "Généraliste", "Orthopédiste"};
+        Instant debut = Instant.now();
 
-            String nomPatient = "toto";
-            int agePatient = 24;
-            boolean aRendezVous = true;
+        String[] nomsPraticiens = {"Dr. Alice Martin", "Dr. Li Wei", "Dr. Maria Gonzalez", "Dr. John Smith", "Dr. Adebayo Adeyemi", "Dr. Hiroshi Yamamoto", "Dr. Elena Ivanova"};
+        String[] specialitesPraticiens = {"Cardiologue", "Dermatologue", "Pédiatre", "Neurologue", "Gynécologue", "Généraliste", "Orthopédiste"};
+
+        for(int i=0; i<1000000000; i++) {
 
             int indexSpecialite = random.nextInt(specialitesPraticiens.length);
             String specialite = specialitesPraticiens[indexSpecialite];
-            afficherMessage("Nouveau rendez-vous demandé pour un %s.".formatted(specialite));
 
             int indexPraticien = trouverPraticienParSpecialite(specialitesPraticiens, specialite);
-            if (indexPraticien == -1) {
-                afficherMessage("Aucun praticien disponible pour la spécialité demandée.");
-            } else {
-                String nomMedecin = nomsPraticiens[indexPraticien];
-                double coutRendezVous = definirCoutDuRendezVous(specialite);
-                afficherMessage("Le patient souhaite prendre un rendez-vous avec un %s.".formatted(specialite));
-
-                afficherInformationsDuPatient(nomPatient, agePatient);
-                afficherRendezVousDuPatient(aRendezVous, nomMedecin, specialite, coutRendezVous);
-            }
+            String nomPraticien = nomsPraticiens[indexPraticien];
+            definirCoutDuRendezVous(specialite);
         }
+
+        Instant fin = Instant.now();
+        Duration duree = Duration.between(debut, fin);
+        long secondes = duree.getSeconds() % 60;
+        long millis = duree.toMillis() % 1000;
+        System.out.printf("Durée de l'opération : %ds %dms", secondes, millis);
     }
 
     private static int trouverPraticienParSpecialite(String[] specialitesPraticiens, String specialite) {
@@ -40,23 +37,7 @@ public class App {
                 return i;
             }
         }
-        return -1; // Retourne -1 si aucun praticien n'est trouvé pour la spécialité donnée
-    }
-
-    private static void afficherRendezVousDuPatient(boolean aRendezVous, String nomMedecin, String specialite, double coutRendezVous) {
-        if (aRendezVous) {
-            afficherMessage(String.format("Le patient a un rendez-vous avec le %s", specialite));
-        } else {
-            afficherMessage("Le patient n'a pas de rendez-vous.");
-        }
-        afficherMessage(String.format("Médecin : %s (%s)", nomMedecin, specialite));
-        afficherMessage(String.format("Coût du rendez-vous : %.2f euros", coutRendezVous));
-    }
-
-    private static void afficherInformationsDuPatient(String nomPatient, int agePatient) {
-        afficherMessage(String.format("Patient : %s", nomPatient));
-        afficherMessage(String.format("Âge : %d ans", agePatient));
-        afficherTypeDePatient(agePatient);
+        return -1;
     }
 
     private static double definirCoutDuRendezVous(String specialite) {
@@ -78,24 +59,6 @@ public class App {
             default:
                 return 70.0;
         }
-    }
-
-    private static void afficherTypeDePatient(int agePatient) {
-        if (agePatient >= 65) {
-            afficherMessage("Le patient est un senior.");
-        } else if (agePatient >= 18) {
-            afficherMessage("Le patient est un adulte.");
-        } else {
-            afficherMessage("Le patient est un mineur.");
-        }
-    }
-
-    private static void afficherMessageDeBienvenue() {
-        afficherMessage("Bienvenue sur DoctoCrest !");
-    }
-
-    private static void afficherMessage(String message) {
-        //System.out.println(message);
     }
 
 }
