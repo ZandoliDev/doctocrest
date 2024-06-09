46ad4ce2e757c7ee4ef0b765cf2c1da07ca36908
Didier Erin <didier.erin@erintek.com>
2024-06-09 22:52:03 +0200

    concepts de base : traitement en masse * utilisation d'un enum pour les praticiens

--- src/main/java/fr/zandolidev/App.java
+++ src/main/java/fr/zandolidev/App.java
@@ -2,34 +2,56 @@ package fr.zandolidev;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Map;
+import java.util.EnumMap;
 import java.util.Random;
 
 public class App {
+
+    enum Praticien {
+        CARDIOLOGUE,
+        DERMATOLOGUE,
+        PEDIATRE,
+        NEUROLOGUE,
+        GYNECOLOGUE,
+        GENERALISTE,
+        ORTHOPEDISTE,
+        ;
+
+    }
+
     public static void main(String[] args) {
+
+        EnumMap<Praticien, String> mapNomsPraticiens = new EnumMap<>(Praticien.class);
+        mapNomsPraticiens.put(Praticien.CARDIOLOGUE, "Dr. Alice Martin");
+        mapNomsPraticiens.put(Praticien.DERMATOLOGUE, "Dr. Li Wei");
+        mapNomsPraticiens.put(Praticien.PEDIATRE, "Dr. Maria Gonzalez");
+        mapNomsPraticiens.put(Praticien.NEUROLOGUE, "Dr. John Smith");
+        mapNomsPraticiens.put(Praticien.GYNECOLOGUE, "Dr. Adebayo Adeyemi");
+        mapNomsPraticiens.put(Praticien.GENERALISTE, "Dr. Hiroshi Yamamoto");
+        mapNomsPraticiens.put(Praticien.ORTHOPEDISTE, "Dr. Elena Ivanova");
+
+        EnumMap<Praticien, Double> mapCoutsRendezVous = new EnumMap<>(Praticien.class);
+        mapCoutsRendezVous.put(Praticien.CARDIOLOGUE, 100.0);
+        mapCoutsRendezVous.put(Praticien.DERMATOLOGUE, 80.0);
+        mapCoutsRendezVous.put(Praticien.PEDIATRE, 90.0);
+        mapCoutsRendezVous.put(Praticien.NEUROLOGUE, 120.0);
+        mapCoutsRendezVous.put(Praticien.GYNECOLOGUE, 110.0);
+        mapCoutsRendezVous.put(Praticien.GENERALISTE, 60.0);
+        mapCoutsRendezVous.put(Praticien.ORTHOPEDISTE, 100.0);
+
 
         Random random = new Random();
 
         Instant debut = Instant.now();
 
-        Map<String, String> mapNomPraticienParSpecialite = Map.of(
-                "Cardiologue", "Dr. Alice Martin",
-                "Dermatologue", "Dr. Li Wei",
-                "Pédiatre", "Dr. Maria Gonzalez",
-                "Neurologue", "Dr. John Smith",
-                "Gynécologue", "Dr. Adebayo Adeyemi",
-                "Généraliste", "Dr. Hiroshi Yamamoto",
-                "Orthopédiste", "Dr. Elena Ivanova"
-        );
-        String[] specialitesPraticiens = {"Cardiologue", "Dermatologue", "Pédiatre", "Neurologue", "Gynécologue", "Généraliste", "Orthopédiste"};
-
         for(int i=0; i<1000000000; i++) {
 
-            int indexSpecialite = random.nextInt(specialitesPraticiens.length);
-            String specialite = specialitesPraticiens[indexSpecialite];
+            Praticien[] praticiens = Praticien.values();
+            int indexSpecialite = random.nextInt(praticiens.length);
+            Praticien specialite = praticiens[indexSpecialite];
 
-            String nomPraticien = mapNomPraticienParSpecialite.get(specialite);
-            definirCoutDuRendezVous(specialite);
+            mapNomsPraticiens.get(specialite);
+            mapCoutsRendezVous.get(specialite);
         }
 
         Instant fin = Instant.now();
@@ -37,27 +59,6 @@ public class App {
         long secondes = duree.getSeconds() % 60;
         long millis = duree.toMillis() % 1000;
         System.out.printf("Durée de l'opération : %ds %dms", secondes, millis);
-    }
-
-    private static double definirCoutDuRendezVous(String specialite) {
-        switch (specialite) {
-            case "Cardiologue":
-                return 100.0;
-            case "Dermatologue":
-                return 80.0;
-            case "Pédiatre":
-                return 90.0;
-            case "Neurologue":
-                return 120.0;
-            case "Gynécologue":
-                return 110.0;
-            case "Généraliste":
-                return 60.0;
-            case "Orthopédiste":
-                return 100.0;
-            default:
-                return 70.0;
-        }
     }
 
 }
